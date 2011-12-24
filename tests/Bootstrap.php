7d646293a5c411a487d4777bd80594dc00dc1155
Rostislav Mykhajliw <necromant2005@gmail.com>
2011-12-24 13:53:41 +0200

    add tests

---
+++ tests/Bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once __DIR__ . '/../library/Zend/View/Helper.php';
+require_once __DIR__ . '/../library/Zend/View/Helper/AbstractHelper.php';
+require_once __DIR__ . '/../library/Zend/View/Helper/Flot.php';
+
